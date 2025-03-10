740a0c968da76401f0b45a7fbbd088610334da32
amy <chiangyenhui@gmail.com>
2025-03-10 16:37:49 -0700

    modify carousel

--- app/components/Carousel.js
+++ app/components/Carousel.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import { useState } from "react";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import Link from "next/link";
 
@@ -10,59 +8,64 @@ export default function Carousel({
   className,
 }) {
   const handleNextSlide = () => {
-    let newSlide = currentSlide === images.length - 1 ? 0 : currentSlide + 1;
-    setCurrentSlide(newSlide);
+    setCurrentSlide((prev) => (prev === images.length - 1 ? 0 : prev + 1));
   };
 
   const handlePrevSlide = () => {
-    let newSlide = currentSlide === 0 ? images.length - 1 : currentSlide - 1;
-    setCurrentSlide(newSlide);
+    setCurrentSlide((prev) => (prev === 0 ? images.length - 1 : prev - 1));
   };
 
   return (
-    // <div className="flex flex-col">
-    <div className={className}>
-      <AiOutlineLeft
-        onClick={handlePrevSlide}
-        className=" left-0 m-auto  inset-y-1/2 cursor-pointer text-gray-400 z-20"
-      />
-      <div className="relative  w-full overflow-hidden  rounded-2xl">
-        {images.map((image, index) => {
-          if (index === currentSlide) {
-            return (
-              <Link href={image.link} target="_blank" key={image.id}>
+    <div className={`flex flex-col relative overflow-hidden ${className}`}>
+      <div>
+        {/* Left Arrow */}
+        <AiOutlineLeft
+          onClick={handlePrevSlide}
+          className="absolute left-2 top-1/2 -translate-y-1/2 cursor-pointer text-gray-400 z-20 text-3xl hover:text-gray-700"
+        />
+
+        {/* Carousel Wrapper */}
+        <div className="relative w-full overflow-hidden">
+          <div
+            className="flex transition-transform duration-500 ease-in-out"
+            style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+          >
+            {images.map((image) => (
+              <Link
+                href={image.link}
+                target="_blank"
+                key={image.id}
+                className="w-full flex-shrink-0"
+              >
                 <img
                   src={image.src}
                   alt={image.alt}
-                  className="animate-fadeIn "
+                  className="w-full h-auto object-cover rounded-2xl"
                 />
               </Link>
-            );
-          }
-        })}
+            ))}
+          </div>
+        </div>
+
+        {/* Right Arrow */}
+        <AiOutlineRight
+          onClick={handleNextSlide}
+          className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer text-gray-400 z-20 text-3xl hover:text-gray-700"
+        />
       </div>
-      <AiOutlineRight
-        onClick={handleNextSlide}
-        className=" right-0 m-auto  inset-y-1/2 cursor-pointer text-gray-400 z-20"
-      />
-      {/* </div> */}
-      {/* <div className="flex justify-center w-full ">
-        {images.map((_, index) => {
-          return (
-            <div
-              className={
-                index === currentSlide
-                  ? "h-4 w-4 bg-gray-700 rounded-full mx-2 mb-2 cursor-pointer"
-                  : "h-4 w-4 bg-gray-300 rounded-full mx-2 mb-2 cursor-pointer"
-              }
-              key={index}
-              onClick={() => {
-                setCurrentSlide(index);
-              }}
-            />
-          );
-        })}
-      </div> */}
+
+      {/* Navigation Dots */}
+      <div className="flex justify-center mt-3">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            onClick={() => setCurrentSlide(index)}
+            className={`h-3 w-3 mx-1 rounded-full cursor-pointer transition-all ${
+              index === currentSlide ? "bg-gray-700 w-4" : "bg-gray-300"
+            }`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
